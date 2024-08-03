c78b6b7e36f22c3eeec01ec1e6edfdbcea0bcc45
TongTong <tongtong950313@163.com>
2024-08-03 13:26:44 +0800

    add torch.no_grad()

--- infer.py
+++ infer.py
@@ -23,36 +23,38 @@ def infer(
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model'])
 
-    # 无条件生成图片
-    for i in range(num_imgs):
-        print(f'Generating {i}th image...')
-        # Euler法间隔
-        dt = 1.0 / step
+    # with torch.no_grad():  # 无需梯度，加速，降显存
+    with torch.no_grad():
+        # 无条件生成图片
+        for i in range(num_imgs):
+            print(f'Generating {i}th image...')
+            # Euler法间隔
+            dt = 1.0 / step
 
-        # 初始的x_t就是x_0，标准高斯噪声
-        x_t = torch.randn(1, 1, 28, 28).to('cuda')
+            # 初始的x_t就是x_0，标准高斯噪声
+            x_t = torch.randn(1, 1, 28, 28).to('cuda')
 
-        for j in range(step):
-            if j % 10 == 0:
-                print(f'Generating {i}th image, step {j}...')
-            t = j * dt
-            t = torch.tensor([t]).to('cuda')
+            for j in range(step):
+                if j % 10 == 0:
+                    print(f'Generating {i}th image, step {j}...')
+                t = j * dt
+                t = torch.tensor([t]).to('cuda')
 
-            v_pred = model(x_t, t)
+                v_pred = model(x_t, t)
 
-            # 使用Euler法计算下一个时间步长的值
-            x_t = rf.euler(x_t, v_pred, dt)
+                # 使用Euler法计算下一个时间步长的值
+                x_t = rf.euler(x_t, v_pred, dt)
 
-        # 最后一步的x_t就是生成的图片
-        # 先去掉batch维度
-        x_t = x_t[0]
-        # 归一化到0到1
-        # x_t = (x_t / 2 + 0.5).clamp(0, 1)
-        x_t = x_t.clamp(0, 1)
-        img = x_t.detach().cpu().numpy()
-        img = img[0] * 255
-        img = img.astype('uint8')
-        cv2.imwrite(os.path.join(save_path, f'{i}.png'), img)
+            # 最后一步的x_t就是生成的图片
+            # 先去掉batch维度
+            x_t = x_t[0]
+            # 归一化到0到1
+            # x_t = (x_t / 2 + 0.5).clamp(0, 1)
+            x_t = x_t.clamp(0, 1)
+            img = x_t.detach().cpu().numpy()
+            img = img[0] * 255
+            img = img.astype('uint8')
+            cv2.imwrite(os.path.join(save_path, f'{i}.png'), img)
 
 
 if __name__ == '__main__':
